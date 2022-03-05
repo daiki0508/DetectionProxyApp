9dcc7102612ca84ad3b9810a682beb26d0fcb0d5
daiki0508 <oochani05085@outlook.jp>
2022-03-05 15:20:19 +0900

    refactor: Change package configuration.

--- app/src/main/java/com/websarva/wings/android/detectionproxyapp/ui/MainActivity.kt
+++ app/src/main/java/com/websarva/wings/android/detectionproxyapp/ui/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.websarva.wings.android.detectionproxyapp
+package com.websarva.wings.android.detectionproxyapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.websarva.wings.android.detectionproxyapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
